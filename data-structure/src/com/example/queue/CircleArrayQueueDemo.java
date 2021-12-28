82deee0e04e242d742bf07c933a9770ddd0e5706
PlusQi <614671436@qq.com>
2021-12-28 22:16:07 +0800

    add:数组模拟环形队列

--- data-structure/src/com/example/queue/CircleArrayQueueDemo.java
+++ data-structure/src/com/example/queue/CircleArrayQueueDemo.java
@@ -1,10 +1,11 @@
 package com.example.queue;
 
+import java.sql.Array;
 import java.util.Scanner;
 
-public class ArrayQueueDemo {
+public class CircleArrayQueueDemo {
     public static void main(String[] args) {
-        ArrayQueue arrayQueue = new ArrayQueue(3);
+        CircleArray arrayQueue = new CircleArray(4);
         char key = ' '; // 接受输入
         Scanner scanner = new Scanner(System.in);
         boolean loop = true;
@@ -49,73 +50,94 @@ public class ArrayQueueDemo {
     }
 }
 
-/**
- * 使用数组模拟队列
- */
-class ArrayQueue {
-    private int maxSize; // 表示该数组队列的最大值
+class CircleArray {
+    private int maxSize;
     private int front; // 队列头
     private int rear; // 队列尾
-    private int[] array; // 模拟队列
+    private int[] arr; // 存放数据，模拟队列
 
-    // 创建队列的构造方法
-    public ArrayQueue(int arrayMaxSize) {
-        maxSize = arrayMaxSize;
-        array = new int[maxSize];
-        front = -1; // 指向队列头部，分析出front是指向队列头的前一个位置
-        rear = -1; // 指向队列尾，指向队列尾的数据(即就是队列最后一个数据)
+    public CircleArray(int arrMaxSize) {
+        this.maxSize = arrMaxSize;
+        this.front = 0;
+        this.rear = 0;
+        this.arr = new int[maxSize];
     }
 
-    // 判断队列是否满
+    /**
+     * 判断队列是否已满
+     * @return
+     */
     public boolean isFull() {
-        return rear == maxSize - 1;
+        return (rear + 1) % maxSize == front;
     }
 
-    // 判断队列是否为空
+    /**
+     * 判断队列是否为空
+     * @return
+     */
     public boolean isEmpty() {
         return rear == front;
     }
 
-    // 往队列里添加数据
+    /**
+     * 添加数据到队列
+     * @param n 添加的数据
+     */
     public void addQueue(int n) {
-        // 判断队列是否满
+        // 判断队列是否已满
         if (isFull()) {
-            throw new RuntimeException("队列已满，无法添加数据");
+            System.out.println("队列满，不能添加数据");
+            return;
         }
-        rear++; // rear后移
-        array[rear] = n;
+
+        // 直接将数据加入
+        arr[rear] = n;
+
+        // rear后移
+        rear = (rear + 1) % maxSize;
     }
 
-    // 获取队列数据，出队列
+    public int size() {
+        return (rear + maxSize - front) % maxSize;
+    }
+
+    /**
+     * 获取队列的数据，出队列
+     * @return
+     */
     public int getQueue() {
         // 判断队列是否为空
         if (isEmpty()) {
-            throw new RuntimeException("队列为空，无法获取数据");
+            System.out.println("队列空不能取数据");
         }
-        front++; // front后移
-        return array[front];
+
+        int value = arr[front];
+        front = (front + 1) % maxSize;
+        return value;
     }
 
-    // 显示队列的所有数据
+    /**
+     * 显示队列的所有数据
+     */
     public void showQueue() {
-        // 遍历
-        if (isEmpty()) {
+        if (isEmpty()){
             System.out.println("队列为空");
-            return;
         }
-        for (int i = 0; i < array.length; i++) {
-            System.out.printf("array[%d]=%d\n", i, array[i]);
+        // 从front开始遍历，遍历多少个元素
+        for (int i = front; i < front + size(); i++) {
+            System.out.printf("arr[%d] = %d\n", i % maxSize, arr[i % maxSize]);
         }
     }
 
-    // 显示队列的头数据
+    /**
+     * 显示队列头数据
+     * @return 队列头数据
+     */
     public int headQueue() {
         if (isEmpty()) {
-            throw new RuntimeException("队列为空");
+            System.out.println("队列空");
         }
 
-        return array[front + 1];
+        return arr[front];
     }
 }
-
-
